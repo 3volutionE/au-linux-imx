boundarydevices/linux

regulator: add define REGULATOR_EVENT_ENABLE

include/linux/regulator/consumer.h
@@ -107,6 +107,7 @@ struct notifier_block;
 #define REGULATOR_EVENT_DISABLE 		0x80
 #define REGULATOR_EVENT_PRE_DISABLE		0x100
 #define REGULATOR_EVENT_PRE_ENABLE		0x200
+#define REGULATOR_EVENT_ENABLE			0x400
 
 struct regulator;
 

3volutionE/au-linux-imx

include/linux/regulator/consumer.h
@@ -121,6 +121,7 @@ struct regmap;
 #define REGULATOR_EVENT_ABORT_DISABLE		0x800
 #define REGULATOR_EVENT_PRE_DO_ENABLE		0x1000
 #define REGULATOR_EVENT_PRE_DO_DISABLE		0x2000
+#define REGULATOR_EVENT_ENABLE			0x4000
 
 /**
  * struct pre_voltage_change_data - Data sent with PRE_VOLTAGE_CHANGE event