smallika/linux-imx6

pci-imx6: allow external clock for imx8M
Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

drivers/pci/host/pci-imx6.c
@@ -1121,8 +1121,12 @@ static void imx6_pcie_init_phy(struct imx6_pcie *imx6_pcie)
 			val = IOMUXC_GPR16;
 
 		regmap_update_bits(imx6_pcie->iomuxc_gpr, val,
-				IMX8MQ_GPR_PCIE_REF_USE_PAD,
-				IMX8MQ_GPR_PCIE_REF_USE_PAD);
+			IMX8MQ_GPR_PCIE_REF_USE_PAD,
+			(imx6_pcie->ext_osc) ? IMX8MQ_GPR_PCIE_REF_USE_PAD : 0);
+		ret = clk_prepare_enable(imx6_pcie->pcie_ext_src);
+		if (ret)
+			dev_err(imx6_pcie->pp.dev,
+					"unable to enable pcie_ext_src clock\n");
 	} else if (imx6_pcie->variant == IMX7D) {
 		/* Enable PCIe PHY 1P0D */
 		regulator_set_voltage(imx6_pcie->pcie_phy_regulator,
@@ -1315,6 +1319,7 @@ static void pci_imx_clk_disable(struct device *dev)
 
 		regmap_update_bits(imx6_pcie->iomuxc_gpr, val,
 				IMX8MQ_GPR_PCIE_REF_USE_PAD, 0);
+		clk_disable_unprepare(imx6_pcie->pcie_ext_src);
 		break;
 	case IMX8QXP:
 	case IMX8QM:
@@ -2271,6 +2276,15 @@ static int imx6_pcie_probe(struct platform_device *pdev)
 			dev_err(&pdev->dev,
 				"imx8mq pcie phy src missing or invalid\n");
 			return PTR_ERR(imx6_pcie->reg_gpc);
+		}
+		imx6_pcie->pcie_ext_src = devm_clk_get(&pdev->dev,
+				"pcie_ext_src");
+		if (IS_ERR(imx6_pcie->pcie_ext_src)) {
+			if (PTR_ERR(imx6_pcie->pcie_ext_src) == -EPROBE_DEFER)
+				return PTR_ERR(imx6_pcie->pcie_ext_src);
+			imx6_pcie->pcie_ext_src = NULL;
+			dev_info(&pdev->dev,
+				"pcie_ext_src clk src missing or invalid\n");
 		}
 	} else if (imx6_pcie->variant == IMX6SX) {
 		imx6_pcie->pcie_inbound_axi = devm_clk_get(&pdev->dev,

3volutionE/au-linux-imx

drivers/pci/host/pci-imx6.c
@@ -1254,6 +1254,10 @@ static void imx_pcie_init_phy(struct imx_pcie *imx_pcie)
 				dev_info(imx_pcie->pp.dev,
 					"PHY Initialization End!.\n");
 			}
+			ret = clk_prepare_enable(imx_pcie->pcie_ext_src);
+			if (ret)
+				dev_err(imx_pcie->pp.dev,
+					"unable to enable pcie_ext_src clock\n");
 		} else {
 			regmap_update_bits(imx_pcie->iomuxc_gpr, val,
 					IMX8MQ_GPR_PCIE_REF_USE_PAD,
@@ -1521,6 +1525,7 @@ static void pci_imx_clk_disable(struct device *dev)
 		regmap_update_bits(imx_pcie->iomuxc_gpr, val,
 				IMX8MQ_GPR_PCIE_CLK_REQ_OVERRIDE_EN,
 				0);
+		clk_disable_unprepare(imx_pcie->pcie_ext_src);
 		break;
 	case IMX8QXP:
 	case IMX8QM:
@@ -2496,7 +2501,7 @@ static int imx_pcie_probe(struct platform_device *pdev)
 	} else if (imx_pcie->variant == IMX8MQ || imx_pcie->variant == IMX8MM) {
 		imx_pcie->iomuxc_gpr =
 			 syscon_regmap_lookup_by_compatible
-			 ("fsl,imx7d-iomuxc-gpr");
+			 ("fsl,imx8mq-iomuxc-gpr");
 		imx_pcie->reg_src =
 			 syscon_regmap_lookup_by_compatible("fsl,imx8mq-src");
 		if (IS_ERR(imx_pcie->reg_src)) {
@@ -2510,6 +2515,15 @@ static int imx_pcie_probe(struct platform_device *pdev)
 			dev_err(&pdev->dev,
 				"imx8mq pcie phy src missing or invalid\n");
 			return PTR_ERR(imx_pcie->reg_gpc);
+		}
+		imx_pcie->pcie_ext_src = devm_clk_get(&pdev->dev,
+				"pcie_ext_src");
+		if (IS_ERR(imx_pcie->pcie_ext_src)) {
+			if (PTR_ERR(imx_pcie->pcie_ext_src) == -EPROBE_DEFER)
+				return PTR_ERR(imx_pcie->pcie_ext_src);
+			imx_pcie->pcie_ext_src = NULL;
+			dev_info(&pdev->dev,
+				"pcie_ext_src clk src missing or invalid\n");
 		}
 	} else if (imx_pcie->variant == IMX6SX) {
 		imx_pcie->pcie_inbound_axi = devm_clk_get(&pdev->dev,